cc4a35ff0fbe5ebff35d387e1dd8809111b6f00e
NatsukiH <natsu.0l7e2o7.s2@gmail.com>
2020-12-23 00:42:01 +0900

    react-router-domの導入

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Topic from './Topic';
 import reportWebVitals from './reportWebVitals';
 
+import {BrowserRouter, Route} from 'react-router-dom';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <BrowserRouter>
+  <Route exact path={'/'} component={App}/>
+  <Route  path={'/topic'}  component={Topic}/>
+  </BrowserRouter>
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
