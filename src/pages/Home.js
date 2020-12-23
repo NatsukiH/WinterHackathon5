31870b749fb4c816615f3ac184b35a44d6e41ee4
NatsukiH <natsu.0l7e2o7.s2@gmail.com>
2020-12-23 21:01:43 +0900

    Homeページのボタン作成と調整

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,53 @@
 import "../App.css";
 import Header from "../components/Header";
+import { Grid, Box, Button } from "@material-ui/core";
+import Logo from "../images/logo.png";
+
+const LogoComponent = () => {
+  return <img src={Logo} alt="ロゴ" width="40%" />;
+};
 
 function Home() {
   return (
     <div className="App">
-      <Header />
+      <header className="App-header">
+        <LogoComponent />
+      </header>
+      <Grid container alignItems="center" justify="center" spacing={5}>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="primary"
+            href="/topic"
+            size="large"
+            style={{ backgroundColor: "#298D1A", width: "30vw" }}
+          >
+            新しい話題を振る
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="primary"
+            href="/escape"
+            size="large"
+            style={{ backgroundColor: "#298D1A", width: "30vw" }}
+          >
+            脱出する
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="primary"
+            href="/laugh"
+            size="large"
+            style={{ backgroundColor: "#298D1A", width: "30vw" }}
+          >
+            笑いを取る
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
