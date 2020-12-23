1d6e470f899d8da1287f4c8a8750bd4bea7aa1a7
NatsukiH <natsu.0l7e2o7.s2@gmail.com>
2020-12-23 23:44:30 +0900

    ファビコンとタイトルの変更

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import "../App.css";
-import Header from "../components/Header";
+// import Header from "../components/Header";
 import { Grid, Typography, Button } from "@material-ui/core";
 import Logo from "../images/logo.png";
 
