59147fd87d94c3ea8a9d9c2554ae29f44254cfc0
NatsukiH <natsu.0l7e2o7.s2@gmail.com>
2020-12-26 00:28:50 +0900

    Topicスロットの内容追加

--- src/components/TopicSlot.js
+++ src/components/TopicSlot.js
@@ -3,25 +3,46 @@ import AutorenewRoundedIcon from "@material-ui/icons/AutorenewRounded";
 
 function TopicSlot() {
   var TopicArray = [
-    "あいうえお",
-    "かきくけこ",
-    "させすせそ",
-    "たちつてと",
-    "なにぬねの",
+    "最近学校どう？",
+    "周りにコロナになった人いる？",
+    "最近学校行ってる？",
+    "最近何してる？",
+    "最近の趣味は？",
+    "最近家で何してる？",
+    "おすすめのアニメは？",
+    "おすすめのドラマは？",
+    "おすすめのアーティストは？",
+    "おすすめの曲は？",
+    "好きなお酒は？",
+    "好きなアイドルは？",
+    "好きな俳優・女優は？",
+    "最近どこに行った？",
+    "おすすめのスポットを教えて！",
+    "北海道と沖縄だったらどっちに行きたい？",
+    "海外に行ったことある？",
+    "旅行に行くならどこに行きたい？",
+    "おすすめのアイテムを教えて！",
+    "これ買ってよかったっていうものを教えて！",
+    "好きな香りは？",
+    "おすすめのカフェは？",
+    "おすすめのお取り寄せグルメを教えて！",
+    "おすすめのデートスポットは？",
+    "面白いエピソードを話して！",
+    "一発ギャグをどうぞ！",
+    "とりあえず写真撮ろうか",
+    "おすすめのゲームは？",
+    "最近寝てる？",
+    "どんな時に至福のひと時を感じる？",
   ]; // 画像ファイル名の設定
   var Flg = 0; // スタートボタンフラグ (0:動いていない 1:動いている)
   var count = 0;
 
   window.setInterval(function () {
     if (Flg == 1) {
-      if (count !== TopicArray.length - 1) {
-        document.querySelector(".slot1").textContent = TopicArray[count++];
-      } else {
-        count = 0;
-        document.querySelector(".slot1").textContent = TopicArray[count++];
-      }
+      var random = Math.floor(Math.random() * TopicArray.length);
+      document.querySelector(".slot1").textContent = TopicArray[random];
     }
-  }, 100);
+  }, 70);
 
   function myStart() {
     if (Flg == 0) {
@@ -29,7 +50,7 @@ function TopicSlot() {
       document.querySelector(".btn").textContent = "Stop";
     } else {
       Flg = 0;
-      document.querySelector(".btn").textContent = "Start";
+      document.querySelector(".btn").textContent = "もう一度回す";
     }
   }
 
@@ -41,7 +62,7 @@ function TopicSlot() {
             style={{
               width: "40vw",
               padding: "15px",
-              border: "4px solid #298D1A",
+              border: "5px solid #298D1A",
             }}
           >
             <Typography variant="h6">
@@ -59,7 +80,7 @@ function TopicSlot() {
           startIcon={<AutorenewRoundedIcon />}
           style={{ backgroundColor: "#298D1A" }}
         >
-          <div class="btn">Start</div>
+          <div class="btn">話題スロットStart</div>
         </Button>
       </Grid>
     </Grid>
